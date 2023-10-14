ba6962b2862419f86dd44b3195b3b2cb32bb8ee2
Nicolas Cuffia-Bergeron <129567055+abxzok379@users.noreply.github.com>
2023-10-14 00:21:39 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+ <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <script src="script.js"></script>
+        <title>Archandalf Website</title>
+
+
+    </head>
+    <body>
+    <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>       
+ </body>
+ </html>
+
+ 
